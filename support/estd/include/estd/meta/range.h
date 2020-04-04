afe27130bd415563876fa1d5a8437702a83d6492
dreamonly <dreamonlysh@gmail.com>
2020-04-05 00:21:06 +0800

    Extend char_enumerate_slice

--- support/estd/include/estd/meta/range.h
+++ support/estd/include/estd/meta/range.h
@@ -1,5 +1,6 @@
 #ifndef ESTD_META_RANGE_H
 #define ESTD_META_RANGE_H
+#include <initializer_list>
 
 namespace estd { namespace meta {
 
@@ -11,7 +12,6 @@ public:
 
   constexpr range_slice(T first, T last)
       : first(first), last(last) {}
-  ~range_slice() = default;
 
   constexpr value_type begin() const { return first; }
   constexpr value_type end() const { return last; }
@@ -25,6 +25,25 @@ private:
 constexpr auto char_slice_number = range_slice<char>('0', '9' + 1);
 constexpr auto char_slice_lower_alphabet = range_slice<char>('a', 'z' + 1);
 constexpr auto char_slice_upper_alphabet = range_slice<char>('A', 'Z' + 1);
+
+template <typename T>
+class enumerate_slice {
+public:
+  using value_type = T;
+  using size_type = typename std::initializer_list<T>::size_type;
+
+  constexpr enumerate_slice(std::initializer_list<T> init)
+    : data(init) {}
+
+  constexpr auto begin() const { return data.begin(); }
+  constexpr auto end() const { return data.end(); }
+  constexpr size_type size() const { return data.size(); }
+
+private:
+  std::initializer_list<T> data;
+};
+
+using char_enumerate_slice = enumerate_slice<char>;
 
 }}
 
