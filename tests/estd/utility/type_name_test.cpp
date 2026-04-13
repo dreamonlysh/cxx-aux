74efeeccf9afa4c21e0fc7baa380c78074d4f12f
dreamonly <dreamonlysh@gmail.com>
2026-04-13 12:54:04 +0000

    Sync from upstream

--- tests/estd/utility/type_name_test.cpp
+++ tests/estd/utility/type_name_test.cpp
@@ -4,7 +4,11 @@
 TEST(TypeName, Numeric) {
   ASSERT_EQ(es::type_name<int>(), "int");
   ASSERT_EQ(es::type_name<double>(), "double");
+#if defined(__clang__) || defined(_MSC_VER)
   ASSERT_EQ(es::type_name<uint16_t>(), "unsigned short");
+#elif defined(__GNUC__)
+  ASSERT_EQ(es::type_name<uint16_t>(), "short unsigned int");
+#endif
   ASSERT_EQ(es::type_name(0), "int");
   ASSERT_EQ(es::type_name(0u), "unsigned int");
 }
